e5974e41943094ea1fbe6cee2bf2675434c6060a
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2016-06-14 22:43:42 +0100

    Added class to store in-exam settings

---
+++ Shared/Settings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shared
+{
+    public class Settings
+    {
+        public string CandidateName { get; set; }
+
+        public List<Section> SelectedSections { get; set; }
+
+        public int NumberOfQuestions { get; set; }
+
+        public int TimeLimit { get; set; }
+
+        public QuestionChoice Choice { get; set; }
+    }
+
+    public enum QuestionChoice
+    {
+        SelectedSections,
+        SpecifiedNumber
+    }
+}
