fac6e00fb10beca4e65a8dd4c9cddfdeceb9a0ce
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2017-05-21 22:35:10 +0100

    chore: adds the writer util file

---
+++ Shared/Util/Writer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shared.Util
+{
+    public class Writer
+    {
+        public static bool ToPdf(Exam exam, string filePath)
+        {
+            return true;
+        }
+
+        public static bool ToJson(Exam exam, string filePath)
+        {
+            return true;
+        }
+
+        public static bool ToXml(Exam exam, string filePath)
+        {
+            return true;
+        }
+    }
+}
