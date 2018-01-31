2da4de2e6ddd8938ec1c3e79247d7a161a9f7fc2
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2018-01-31 16:35:31 +0100

    chore: update smd script

--- coverage.cmd
+++ coverage.cmd
@@ -1,2 +1,1 @@
-# run tests
-%xunit20%\xunit.console.x86.exe  ".\tests\Shared.Tests\bin\Debug\Shared.Tests.dll" -appveyor
+%xunit20%\xunit.console  ".\tests\Shared.Tests\bin\Debug\Shared.Tests.dll" -appveyor
