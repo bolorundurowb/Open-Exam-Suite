27f790dfac56a4337612d055e55d3f1b12652f59
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2018-02-01 22:53:48 +0100

    feature: move to using a service collection instead of one class/model implementation

---
+++ src/lib/Storage/Interfaces/IAppSettingsCollection.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Storage.Models;
+
+namespace Storage.Interfaces
+{
+    public interface IAppSettingsCollection
+    {
+        void Add(AppSetting settings);
+        void Clear();
+        IEnumerable<AppSetting> GetAll();
+    }
+}
