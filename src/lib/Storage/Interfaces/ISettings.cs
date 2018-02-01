a725e32ca9bbd0edb17cc99a0c99d683a381bc31
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2018-02-01 15:50:03 +0100

    feature: add the settings interface

---
+++ src/lib/Storage/Interfaces/ISettings.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Storage.Interfaces
+{
+    public interface ISettings
+    {
+        void Save();
+        void Clear();
+        List<ISettings> GetAll();
+    }
+}
