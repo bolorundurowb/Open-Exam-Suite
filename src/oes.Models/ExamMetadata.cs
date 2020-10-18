bfe31d0c35ec87fb7cd6af19b30f29a00de61d3a
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-10-18 13:29:11 +0100

    ft: add in exam metadata

---
+++ src/oes.Models/ExamMetadata.cs
@@ -0,0 +1,37 @@
+﻿namespace oes.Models
+{
+    public class ExamMetadata
+    {
+        public string Title { get; private set; }
+
+        public string Instructions { get; private set; }
+
+        public string Code { get; private set; }
+
+        public int Version { get; private set; }
+
+        public int TimeLimitInMinutes { get; private set; }
+
+        public float MinimumScorePercentage { get; private set; }
+
+        private ExamMetadata()
+        {
+        }
+
+        public ExamMetadata(string title, string instructions, string code, int timeLimitInMinutes,
+            float minimumScorePercentage)
+        {
+            Title = title;
+            Instructions = instructions;
+            Code = code;
+            TimeLimitInMinutes = timeLimitInMinutes;
+            MinimumScorePercentage = minimumScorePercentage;
+            Version = 1;
+        }
+
+        public void BumpVersion()
+        {
+            Version += 1;
+        }
+    }
+}
