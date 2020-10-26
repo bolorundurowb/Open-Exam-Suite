d0a17260a6f23d306d1d829b4245f0e6984d7318
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-10-26 20:31:47 +0100

    ft: add in filke reader interface

--- src/oes.IO/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace oes.IO
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/oes.IO/Interfaces/IFileReader.cs
@@ -0,0 +1,11 @@
+﻿using oes.Models;
+
+namespace oes.IO.Interfaces
+{
+    public interface IFileReader
+    {
+        Exam FromFile(string filePath);
+
+        Exam FromText(string text);
+    }
+}
