2038f7594c62742fbdfb9b8b57f264e4b88e15bd
Winner-Timothy Bolorunduro <bolorundurowb@gmail.com>
2026-03-16 20:25:13 +0100

    ft: update resource manager namespaces for Simulator and Creator apps to align with OpenExamSuite convention

--- src/apps/Simulator/Properties/Resources.Designer.cs
+++ src/apps/Simulator/Properties/Resources.Designer.cs
@@ -36,7 +36,7 @@ namespace OpenExamSuite.Simulator.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Simulator.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("OpenExamSuite.Simulator.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
